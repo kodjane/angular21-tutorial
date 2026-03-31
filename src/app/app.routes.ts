7a0416c83e7b89fd97f9ebbcde0f4cf7086b67e4
Aime Kodjane <aime.kodjane@ehealth.fgov.be>
2026-03-31 21:52:59 +0200

    added routerLink to the nav button + default route + NotFound page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { Variables } from './components/variables/variables';
 import { DataBinding } from './components/data-biding/data-binding.component';
 import { Signal } from './components/signal/signal';
+import { NotFound } from './components/not-found/not-found';
 
 export const routes: Routes = [
+  {path: "", component: Variables, pathMatch:"full"},
   {path: "variables", component: Variables},
   {path: "dataBinding", component: DataBinding},
-  {path: "signals", component: Signal},
+  {path: "signal", component: Signal},
+  {path: "**", component: NotFound}
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Variables } from './components/variables/variables';
 import { DataBinding } from './components/data-biding/data-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
