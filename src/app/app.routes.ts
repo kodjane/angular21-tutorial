a189a901b93f1e0dd07b25ef76258d30990e13fc
Aime Kodjane <aime.kodjane@ehealth.fgov.be>
2026-04-07 10:46:34 +0200

    created User Master component and added routes accordingly

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DataBinding } from './components/data-biding/data-binding.component';
 import { SignalComponent } from './components/signalComponent/signal.component';
 import { NotFound } from './components/not-found/not-found';
 import { ControlFlow } from './components/control-flow/control-flow';
+import { UserMaster } from './components/user-master/user-master';
 
 export const routes: Routes = [
   {path: "", component: Variables, pathMatch:"full"},
@@ -11,5 +12,6 @@ export const routes: Routes = [
   {path: "dataBinding", component: DataBinding},
   {path: "signal", component: SignalComponent},
   {path: "controlFlow", component: ControlFlow},
+  {path: "userMaster", component: UserMaster},
   {path: "**", component: NotFound}
 ];
