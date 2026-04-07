772563737e08c8383d3edaf9d4c978dae893b3f8
Aime Kodjane <aime.kodjane@ehealth.fgov.be>
2026-04-07 13:38:35 +0200

    Added validation on userName

--- src/app/components/user-master/user-master.ts
+++ src/app/components/user-master/user-master.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-master',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-master.html',
   styleUrl: './user-master.css',
 })
-export class UserMaster {}
+export class UserMaster {
+  userObject = {
+    id: 0,
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    city: '',
+    state: '',
+  };
+}
