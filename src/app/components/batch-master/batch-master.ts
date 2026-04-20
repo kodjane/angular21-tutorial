61149d618efce04cba6d45b783ea73146b9bccb4
Aime Kodjane <aime.kodjane@ehealth.fgov.be>
2026-04-20 10:00:28 +0200

    created BatchMaster

--- src/app/components/batch-master/batch-master.ts
+++ src/app/components/batch-master/batch-master.ts
@@ -1,9 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-batch-master',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './batch-master.html',
-  styleUrl: './batch-master.css',
+  styleUrls: ['./batch-master.css'],
 })
-export class BatchMaster {}
+export class BatchMaster {
+  userObject: User = new User();
+
+  httpClient = inject(HttpClient);
+  users: User[] = [];
+
+  constructor() {
+    this.getAllUsers();
+  }
+
+  toggleSaveUser() {
+    this.httpClient.post('https://jsonplaceholder.typicode.com/users', this.userObject).subscribe(
+      (_response) => {
+        // simple success handling
+        alert('User created successfully');
+        // Optionally refresh list
+        this.getAllUsers();
+      },
+      (error: any) => {
+        // error.message is a property
+        alert('User creation failed: ' + (error?.message || error));
+      },
+    );
+  }
+
+  private getAllUsers() {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/users').subscribe((response: any) => {
+      this.users = response || [];
+    });
+  }
+}
+
+export class User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  city: string;
+  state: string;
+
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.username = '';
+    this.email = '';
+    this.password = '';
+    this.city = '';
+    this.state = '';
+  }
+}
